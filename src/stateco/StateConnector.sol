95256acb58195c4b008ea366c7afb12b4ae5a1a2
Sean Rowan <sprwn@protonmail.com>
2021-12-02 19:04:03 -0500

    Optimised voting buffer storage, 80615 gas cost to attest infinite transactions in one step

--- src/stateco/StateConnector.sol
+++ src/stateco/StateConnector.sol
@@ -10,56 +10,42 @@ contract StateConnector {
 // Data Structures
 //====================================================================
 
-    struct AttestationRequest {
-        uint256     attestationWindowEndTime;
-        bytes32     attestationHash;
+    uint256 public constant BUFFER_WINDOW = 90 seconds; // Amount of time a buffer is active before cycling to the next one
+ 
+    // Struct for Vote in round 'R'
+    struct Vote {
+        bytes32 maskedMerkleHash; // Masked hash of the merkle tree that contains valid requests from round 'R-1' 
+        bytes32 committedRandom; // Hash of random value that masks 'maskedMerkleHash' above
+        bytes32 revealedRandom; // Reveal of 'randomCommit' from round 'R-1' Votes struct, used in 'R-2' request voting
     }
 
-    address public constant GENESIS_COINBASE = address(0x0100000000000000000000000000000000000000);
+    struct Buffers {
+        Vote[3] votes; // {Requests, Votes, Reveals}
+        uint256 latestVoteBlockNumber;  // The block.number of the last time each address voted
+    }
 
-    // For CYCLIC_BUFFER_SIZE==2048 it costs 524,288 SGB burned to cycle the buffer,
-    // AND 65043 gas per buffer slot, i.e. 34,101,264,384 gas used to cycle the buffer.
-    // If Flare blocks only contained requestAttestations txs, it would require
-    // 4263 blocks to cycle the buffer.
-
-    // This translates to an approx. lower-bound of 24 minutes required to cycle the buffer;
-    // meaning that from the time a transaction is requested for attestation, a minimum of
-    // 24 minutes is given to be able to both attest the transaction and then prove it to 
-    // another contract using reconstructTransactionProof. This cyclic buffer mechanism
-    // keeps the contract scalable due to bounding the storage used.
-
-    uint256 public constant CYCLIC_BUFFER_SIZE = 2048; // Total number of buffer slots == 256*2048
-
-    // The accumulation of attestations in response to an attestation request is given at minimum
-    // 3 minutes before the vote count on attestations can be checked using the proveTransaction
-    // function. Vote counting happens at the golang level in state_connector.go, and definition
-    // of the valid voting set is locally-defined on every Flare node. This means that every
-    // Flare validator can have a different definition of the voting set.
-
-    uint256 public constant ATTESTATION_WINDOW = 3 minutes;
-
-    // Cyclic index for attestation requests, (0 <= attestationRequestIndex < 256*CYCLIC_BUFFER_SIZE)
-    uint256 public attestationRequestIndex;
-    // Requested underlying chain transactions for attestation
-    AttestationRequest[256*CYCLIC_BUFFER_SIZE] public attestationRequests;
-    // Transaction attestations submitted from any account
-    mapping(address => uint256[CYCLIC_BUFFER_SIZE]) public attestationSubmits;
-    // Last block height checked per account, used for event retrieval
-    mapping(address => uint256) public latestBlockHeightFulfilled;
-    // Proven transactions, attestationRequestIndex => attestationHash
-    bytes32[256*CYCLIC_BUFFER_SIZE] private provenTransactions;
+    mapping(address => Buffers) public buffers;
+    uint256 public totalBuffers; // The total number of buffers that have been used by new attestation requests. 
+                                 // totalBuffers == (block.timestamp / BUFFER_WINDOW)
+                                 // The equation above gives the current buffer number: B, (0 <= B < 3)
+    uint256[3] public earliestBufferBlockNumber; // For the last NUM_VOTING_PHASES buffers, this value defines
+                                                                 // the block.number that each buffer was first used by a
+                                                                 // new attestation request. This value is used for event filtering
+                                                                 // by defining block.number windows for filtering attestation
+                                                                 // request events.
 
 //====================================================================
 // Events
 //====================================================================
 
-    event AttestationRequested(
-        uint256 n,
-        uint64 chainId,
-        uint64 blockHeight,
-        bytes32 txId,
-        uint16 utxo,
-        bytes32 payloadHash
+    // instructions: (uint64 chainId, uint64 blockHeight, uint16 utxo, bool full)
+    // The variable 'full' defines whether to provide the complete transaction details
+    // in the attestation response
+    event AttestationRequest(
+        uint256 timestamp,
+        uint256 bufferNumber,
+        uint256 instructions,
+        bytes32 txId
     );
 
 //====================================================================
@@ -74,145 +60,39 @@ contract StateConnector {
 //====================================================================  
 
     function requestAttestations(
-        uint64 chainId,
-        uint64 blockHeight,
-        bytes32 txId,
-        uint16 utxo,
-        bytes32 payloadHash
-    ) external payable returns (
-        uint256 _n
-    ) {
+        uint256 instructions,
+        bytes32 txId
+    ) external payable {
         // Check for minimum fee burn
         require(msg.value >= 1 ether);
         // Check for empty inputs
-        require(blockHeight > 0);
+        require(instructions > 0);
         require(txId > 0x0);
-        require(payloadHash > 0x0);
-        // Construst attestationHash used for uniquely defining this request
-        bytes32 locationHash = keccak256(abi.encodePacked(
-                keccak256(abi.encodePacked("FlareStateConnector_LOCATION")),
-                keccak256(abi.encodePacked(chainId)),
-                keccak256(abi.encodePacked(blockHeight)),
-                keccak256(abi.encodePacked(txId)),
-                keccak256(abi.encodePacked(utxo))
-            ));
-        bytes32 attestationHash = keccak256(abi.encodePacked(
-            keccak256(abi.encodePacked("FlareStateConnector_ATTESTATION")),
-            locationHash,
-            payloadHash
-        ));
-        // Store the request in the cyclic buffer at the current value of attestationRequestIndex
-        uint256 n = attestationRequestIndex;
-        attestationRequests[n] = AttestationRequest(
-            block.timestamp + ATTESTATION_WINDOW,
-            attestationHash
-        );
-        // Emit an event containing the details of the request, these details are not stored in 
-        // contract storage so they must be retrieved using event retrieval.
-        emit AttestationRequested(n, chainId, blockHeight, txId, utxo, payloadHash);
-        // Increment the attestationRequestIndex, and roll the index back to 0 if at the end of the
-        // cyclic buffer where the updated attestationRequestIndex == (256*CYCLIC_BUFFER_SIZE)
-        attestationRequestIndex = (n + 1)%(256*CYCLIC_BUFFER_SIZE);
 
-        return n;
-    }
-
-    // submitAttestations permits an account to submit 256 attestations in a single commit
-    function submitAttestations(
-        uint256 lowestAttestationRequestNumber,
-        uint256 blockHeightFulfilled,
-        uint256 booleanMap
-    ) external {
-        // Compute location in the cyclic buffer to store the attestations
-        uint256 i = lowestAttestationRequestNumber/256;
-        uint256 j = lowestAttestationRequestNumber%256;
-        // Store the attestations without overwriting old attestations
-        attestationSubmits[msg.sender][i] = attestationSubmits[msg.sender][i] | booleanMap >> j;
-        attestationSubmits[msg.sender][(i+1)%CYCLIC_BUFFER_SIZE] = booleanMap << 256-j;
-        // Update the latest block height on Flare that the sender has checked for 
-        // attestation requests. This is useful for statelessly tracking AttestationRequested
-        // events by always filtering from latestBlockHeightFulfilled[msg.sender] and up
-        latestBlockHeightFulfilled[msg.sender] = blockHeightFulfilled;
-    }
-
-    // getAttestation is used by state_connector.go to retrieve the binary attestation vote
-    // of an account in relation to an attestation request, n, in the cyclic buffer.
-    function getAttestation(
-        uint256 n
-    ) external view returns (
-        bool _attested
-    ) {
-        return (attestationSubmits[msg.sender][n/256] & (uint256(1) << 256-(n%256))) > 0;
-    }
-
-    function proveTransaction(
-        uint256 n,
-        bytes32 attestationHash
-    ) external returns (
-        uint256 _n
-    ) {
-        // Copy the attestation request to memory
-        AttestationRequest memory attestationRequest = attestationRequests[n];
-        // Require that the attestation request exists and that the proveTransaction tx
-        // is not being submitted below the ATTESTATION_WINDOW time.
-        require(attestationRequest.attestationWindowEndTime > 0);
-        require(block.timestamp >= attestationRequest.attestationWindowEndTime);
-        // Verify that the inputted attestationHash matches the request's attestationHash
-        require(attestationRequest.attestationHash == attestationHash);
-        // If the golang-level attestation voting check passes, block.coinbase will be
-        // set to msg.sender, signalling to this function that the attestation request
-        // should be deemed proven and included in provenTransactions
-        if (block.coinbase == msg.sender && block.coinbase != GENESIS_COINBASE) {
-            provenTransactions[n] = attestationHash;
+        uint256 updatedBufferNumber = (block.timestamp / BUFFER_WINDOW);
+        if (updatedBufferNumber > totalBuffers) {
+            earliestBufferBlockNumber[updatedBufferNumber % 3] = block.number;
+            totalBuffers = updatedBufferNumber;
         }
-        return n;
+
+        // Emit an event containing the details of the request, these details are not stored in 
+        // contract storage so they must be retrieved using event filtering.
+        emit AttestationRequest(block.timestamp, totalBuffers, instructions, txId); 
     }
 
-    function reconstructTransactionProof(
-        uint64 chainId,
-        uint64 blockHeight,
-        bytes32 txId,
-        uint16 utxo,
-        bytes32 destinationHash,
-        bytes32 dataHash,
-        uint256 amount,
-        uint256 gasUsed,
-        uint256 errorCode,
-        uint256 n
-    ) external view returns (
-        bool _proven
-    ) {
-        // Reconstruct the locationHash of the transaction
-        bytes32 locationHash = keccak256(abi.encodePacked(
-            keccak256(abi.encodePacked("FlareStateConnector_LOCATION")),
-            keccak256(abi.encodePacked(chainId)),
-            keccak256(abi.encodePacked(blockHeight)),
-            keccak256(abi.encodePacked(txId)),
-            keccak256(abi.encodePacked(utxo))
-        ));
-        // Reconstruct the payloadHash of the transaction. The field 'dataHash' can
-        // itself be reconstructed further as a catch-all field that varies in meaning
-        // for different chains. For example, on Ethereum this could mean explicitly its
-        // 'data' field, however on another chain this could reference a custom currency
-        // code or a file storage reference.
-        bytes32 payloadHash = keccak256(abi.encodePacked(
-            keccak256(abi.encodePacked("FlareStateConnector_PAYLOAD")),
-            destinationHash,
-            dataHash,
-            keccak256(abi.encodePacked(amount)),
-            keccak256(abi.encodePacked(gasUsed)),
-            keccak256(abi.encodePacked(errorCode))
-        ));
-        // Reconstruct the attestationHash which uniquely identifies this underlying chain 
-        // transaction.
-        bytes32 attestationHash = keccak256(abi.encodePacked(
-            keccak256(abi.encodePacked("FlareStateConnector_ATTESTATION")),
-            locationHash,
-            payloadHash
-        ));
-        // Return a boolean defining if this reconstructed transaction proof has been proven
-        // by the state connector contract.
-        return (provenTransactions[n] == attestationHash);
+    function submitAttestation(
+        uint256 bufferNumber,
+        bytes32 maskedMerkleHash,
+        bytes32 committedRandom,
+        bytes32 revealedRandom
+    ) external {
+        require(bufferNumber == block.timestamp / BUFFER_WINDOW);
+        buffers[msg.sender].latestVoteBlockNumber = block.number;
+        buffers[msg.sender].votes[bufferNumber % 3] = Vote(
+            maskedMerkleHash,
+            committedRandom,
+            revealedRandom
+        );
     }
 
 }
