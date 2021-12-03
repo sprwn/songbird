afb1c90b4c907fc9f632fc5e8ace821918e3d2d7
Sean Rowan <sprwn@protonmail.com>
2021-12-03 04:00:58 -0500

    getAttestation() and finaliseRound()

--- src/stateco/StateConnector.sol
+++ src/stateco/StateConnector.sol
@@ -13,19 +13,21 @@ contract StateConnector {
     uint256 public constant BUFFER_TIMESTAMP_OFFSET = 1636070400 seconds; // November 5th, 2021
     uint256 public constant BUFFER_WINDOW = 90 seconds; // Amount of time a buffer is active before cycling to the next one
     uint256 public constant TOTAL_STORED_BUFFERS = 3; // {Requests, Votes, Reveals}
+    uint256 public constant TOTAL_STORED_PROOFS = (1 weeks)/BUFFER_WINDOW; // Store a proof for one week
 
-    // Struct for Vote in round 'R'
-    struct Vote {
+    //=======================
+    // VOTING DATA STRUCTURES
+    //=======================
+
+    struct Vote { // Struct for Vote in round 'R'
         bytes32 maskedMerkleHash; // Masked hash of the merkle tree that contains valid requests from round 'R-1' 
         bytes32 committedRandom; // Hash of random value that masks 'maskedMerkleHash' above
         bytes32 revealedRandom; // Reveal of 'committedRandom' from round 'R-1' Votes struct, used in 'R-2' request voting
     }
-
     struct Buffers {
         Vote[TOTAL_STORED_BUFFERS] votes; // {Requests, Votes, Reveals}
-        uint256 latestVoteBlockNumber;  // The block.number of the last time each address voted
+        uint256 latestVote;  // The latest buffer number that this account has voted on, used for determining relevant votes
     }
-
     mapping(address => Buffers) public buffers;
     uint256 public totalBuffers; // The total number of buffers that have been used by new attestation requests. 
                                  // totalBuffers == (block.timestamp / BUFFER_WINDOW)
@@ -34,6 +36,15 @@ contract StateConnector {
                                                                  // new attestation request. This value is used for event filtering
                                                                  // by defining block.number windows for filtering attestation
                                                                  // request events.
+
+    //======================
+    // MERKLE PROOF DATA STRUCTURES
+    //======================
+
+    uint256 public totalProofs; // The total number of merkle roots that have been proven using finaliseRound()
+    bytes32[TOTAL_STORED_PROOFS] public merkleRoots; // The proven merkle roots for each buffer number,
+                                                     // accessed using: bufferNumber % TOTAL_STORED_PROOFS
+                                                     // within one week of proving the merkle root.
 
 //====================================================================
 // Events
@@ -88,12 +99,36 @@ contract StateConnector {
         bytes32 revealedRandom
     ) external {
         require(bufferNumber == (block.timestamp - BUFFER_TIMESTAMP_OFFSET) / BUFFER_WINDOW);
-        buffers[msg.sender].latestVoteBlockNumber = block.number;
+        buffers[msg.sender].latestVote = bufferNumber;
         buffers[msg.sender].votes[bufferNumber % TOTAL_STORED_BUFFERS] = Vote(
             maskedMerkleHash,
             committedRandom,
             revealedRandom
         );
+    }
+
+    function getAttestation(
+        uint256 bufferNumber
+    ) external view returns (
+        bytes32 _unmaskedMerkleHash
+    ) {
+        require(buffers[msg.sender].latestVote >= bufferNumber);
+        bytes32 revealedRandom = buffers[msg.sender].votes[bufferNumber % TOTAL_STORED_BUFFERS].revealedRandom;
+        bytes32 committedRandom = buffers[msg.sender].votes[(bufferNumber-1) % TOTAL_STORED_BUFFERS].committedRandom;
+        require(committedRandom == keccak256(abi.encodePacked(revealedRandom)));
+        bytes32 maskedMerkleHash = buffers[msg.sender].votes[(bufferNumber-2) % TOTAL_STORED_BUFFERS].maskedMerkleHash;
+        return (maskedMerkleHash ^ revealedRandom);
+    }
+
+    function finaliseRound(
+        uint256 bufferNumber,
+        bytes32 merkleHash
+    ) external {
+        require(bufferNumber == (block.timestamp - BUFFER_TIMESTAMP_OFFSET) / BUFFER_WINDOW);
+        require(bufferNumber > totalProofs);
+        require(msg.sender == block.coinbase); // This function can only be called from the golang code
+        totalProofs = bufferNumber;
+        merkleRoots[bufferNumber % TOTAL_STORED_PROOFS] = merkleHash;
     }
 
 }
